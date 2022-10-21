7259ecd3cbbd91c69893ca0a3d18e6de5e9243e1
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-10-21 04:38:00 +0000

    updates

---
+++ com/cc/java/Bird.java
@@ -0,0 +1,21 @@
+package com.cc.java;
+
+/**
+ * Bird
+ */
+public class Bird extends HoneyBee{
+
+    @Override
+    public String doYourJob() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String fly() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    
+}
